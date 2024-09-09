dba5018014bb9c942c36d3d4ef5b82fe10de3b65
Gurveen <gurveenkaursahni@gmail.com>
2024-09-08 22:07:54 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import cv2
+import matplotlib.pyplot as plt
+import easyocr
+
+
+
+# read the image
+image_path = "images/s-wx-wu7mesh-signs-along-the-sea-to-sky-highway.JPG.avif"
+image= cv2.imread(image_path, cv2.IMREAD_COLOR)
+
+# instanciate text detector
+
+
+# detect the text on the image
+
+# draw bbox and text
